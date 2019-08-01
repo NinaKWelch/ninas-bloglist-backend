1ea7b9b66997be842a93426f1eb5eeb5060af3f5
NinaKWelch <ninakwelch@gmail.com>
2019-08-01 13:32:09 +0100

    tehtava 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,15 +1,25 @@
 const dummy = blogs => (blogs.length + 1) - blogs.length
 
-
-
 const totalLikes = blogs => {
     const reducer = (sum, blog) => sum + blog.likes
     
     return blogs.reduce(reducer, 0)
 }    
 
+const favoriteBlog = blogs => {
+    blogs.sort((a, b) => a.likes - b.likes)
+
+    const blog = blogs[blogs.length - 1]
+
+    return blogs.length === 0 ? {} : { 
+        title: blog.title, 
+        author: blog.author, 
+        likes: blog.likes
+    }
+}
 
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
