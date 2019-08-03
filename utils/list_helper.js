16ad87229161600d254832de243c5218c6e4ee07
NinaKWelch <ninakwelch@gmail.com>
2019-08-03 02:22:06 +0100

    inprove tests

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,4 +1,4 @@
-/** HELPER FUNCTIONS */
+/** FUNCTIONS */
 // group an array of objects into an object
 // whose keys are the shared property and
 // whose key values are an array of objects
@@ -22,86 +22,78 @@ const groupBy = (array, property) => {
     return group
 }
 
-// find author with largest value
-const findAuthorWithMost = (array, value) => {
-    array.sort((a, b) => b[value] - a[value])
-    return array[0]
+// sort objects in an array 
+// from highest value to lowest
+const sortBy = (array, value) => {
+    return array.sort((a, b) => b[value] - a[value])  
 }
 
-/** TESTS */
+// add values of same type 
+// in an array of objects
+const reduceBy = (array, value) => {
+    let reducer = (acc, num) => acc + num[value]
+    
+    return array.reduce(reducer, 0)
+}
+
+// return author with highest of a given value
+const findAuthorWithHigest = (array, value) => {
+    // group blogs by author
+    const groupedBlogsByAuthor = groupBy(array, 'author')
+ 
+    // create an array or authors
+    const authorNames = Object.keys(groupedBlogsByAuthor)
+
+    const authorData = []
+
+    // create an object for each author
+    // and add them to authorData array
+    authorNames.forEach(author => {
+        let blogsByAuthor = groupedBlogsByAuthor[author]
+        let obj = { author }
+
+        if (value === 'blogs') {
+            obj[value] = blogsByAuthor.length
+        }
+
+        if (value === 'likes') {
+            obj[value] = reduceBy(blogsByAuthor, value)
+        }
+
+        authorData.push(obj)  
+    })
+
+    sortBy(authorData, value)
+    return authorData[0]
+}
+
+/** HELPER FUNCTIONS */
 const dummy = blogs => (blogs.length + 1) - blogs.length
 
 const totalLikes = blogs => {
-    let reducer = (sum, blog) => sum + blog.likes
-    
-    return blogs.reduce(reducer, 0)
+    return reduceBy(blogs, 'likes')
 }    
 
 const favoriteBlog = blogs => {
-    blogs.sort((a, b) => a.likes - b.likes)
+    sortBy(blogs, 'likes')
 
-    let blog = blogs[blogs.length - 1]
-
-    return blogs.length === 0 ? {} : { 
-        title: blog.title, 
-        author: blog.author, 
-        likes: blog.likes
+    return blogs.length === 0 ? {} : {
+        title: blogs[0].title, 
+        author: blogs[0].author, 
+        likes: blogs[0].likes         
     }
 }
 
 const mostBlogs = blogs => {
-    if (blogs.length === 0) {
-        return 'no authors'
-    } 
-
-    // group blogs by author
-    const groupedBlogsByAuthor = groupBy(blogs, 'author')
-
-    // create an array or authors
-    const authorNames = Object.keys(groupedBlogsByAuthor)
-
-    let authorData = []
-
-    authorNames.forEach(author => {
-        let blogsByAuthor = groupedBlogsByAuthor[author]
-
-        let obj = {
-            author,
-            blogs: blogsByAuthor.length
-        }
-        authorData.push(obj)  
-    })
-    console.log(authorData)
-    return findAuthorWithMost(authorData, 'blogs')
+    return blogs.length !== 0 ? 
+        findAuthorWithHigest(blogs, 'blogs') : 
+        'no authors'
 }
 
 const mostLikes = blogs => {
-    if (blogs.length === 0) {
-        return 'no likes'
-    } 
-    
-    // group blogs by author
-    const groupedBlogsByAuthor = groupBy(blogs, 'author')
-
-    // create an array or authors
-    const authorNames = Object.keys(groupedBlogsByAuthor)
-
-    let authorData = []
-    
-    authorNames.forEach(author => {
-        let blogsByAuthor = groupedBlogsByAuthor[author]
-
-        let obj = {
-            author,
-            likes: blogsByAuthor.reduce((acc, num) => {
-                return acc + num.likes
-            }, 0)
-        }
-        authorData.push(obj)  
-    })
-
-    console.log(authorData)
-    return findAuthorWithMost(authorData, 'likes')
+    return blogs.length !== 0 ? 
+        findAuthorWithHigest(blogs, 'likes') : 
+        'no likes'
 }
 
 module.exports = {
